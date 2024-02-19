a643329d51c18401a9562fa3a634303b6cdda8f2
0pripyat92 <devjr0000@gmail.com>
2024-02-19 01:07:34 -0300

    commit aula 01

---
+++ Aula01/index.js
@@ -0,0 +1,22 @@
+
+//Console
+console.log(`Hello`)
+console.log(`I love pizza!`)
+
+//alertas
+window.alert(`Hello`)
+window.alert(`I like pizza!`)
+
+//Comenterios
+//this is a commert
+
+/*
+this
+is 
+a
+comment
+*/
+
+//Mudar texto do html com JS
+document.getElementById(`myH1`).textContent = `Hello`;
+document.getElementById(`myP`).textContent = `I like pizza!`
---
+++ Aula01/index.md
@@ -0,0 +1,8 @@
+
+## JavaScript tutorial for beginners 🌐
+
+### console.log
+### window.alert
+### //Comenterios /**/
+### //Mudar texto do html com JS com 
+### document.getElementById(`exemple`).textContent
